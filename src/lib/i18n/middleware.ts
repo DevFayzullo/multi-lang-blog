e763602c66c35e850dd57114262f3bb8da002ae9
DevFayzullo <fayzullo.coder@gmail.com>
2025-10-03 22:03:45 +0900

    middleware.ts file is created

---
+++ src/lib/i18n/middleware.ts
@@ -0,0 +1,11 @@
+import createMiddleware from 'next-intl/middleware';
+
+export default createMiddleware({
+  locales: ['ko', 'en', 'uz'],
+  defaultLocale: 'ko',
+  localePrefix: 'always'
+});
+
+export const config = {
+  matcher: ['/((?!_next|.*\\..*$).*)']
+};
