2217a0d8746bac512073ee504a6a2a55d5a051fb
DevFayzullo <fayzullo.coder@gmail.com>
2025-10-17 10:05:05 +0900

    bugs is fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
-// import '../../styles/globals.css'
+import './globals.css';
+import {getLocale} from 'next-intl/server';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  const locale = await getLocale();
+
   return (
-    <html suppressHydrationWarning>
-      <body>{children}</body>
+    <html lang={locale} suppressHydrationWarning>
+      <body className="min-h-screen bg-white text-gray-900 dark:bg-neutral-900 dark:text-neutral-100">
+        {children}
+      </body>
     </html>
   );
 }
