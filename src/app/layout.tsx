9c0730d0a2d26e78d47a5bef0865eeaa6e1513cd
DevFayzullo <fayzullo.coder@gmail.com>
2026-01-22 14:17:37 +0900

    tsconfig.json file is updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,11 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
